565f978beac55688cbd0c3657ffc90e3c3282694
agrim002 <82631309+agrim002@users.noreply.github.com>
2021-09-28 12:14:45 -0400

    Author view route updated to use author view
    Links to ejs form to create author 
    Adds author view to retrieve all authors

--- routes/authorRoute.js
+++ routes/authorRoute.js
@@ -2,25 +2,16 @@ const express = require('express');
 const router = express.Router();
 Author = require('../schemas/authorSchema');
 
-
-router.get('/create', (req,res) => res.send('Create Author'));
-
+router.get('/create', (req,res) => res.render('CreateAuthor'));
 
 router.get('/view', (req, res) => {
-    Book.get(function (err, Book) {
-        if (err) {
-            res.json({
-                status: "Error",
-                message: err,
-            });
-        }
-        res.json({
-            message: "Books retrieved successfully",
-            data: Book
+    Author.find() 
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    });
 });
-
-
 
 module.exports = router;

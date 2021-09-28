c0c2a5f5aa7e62153f6d860f0ebcb8026c41af1f
agrim002 <82631309+agrim002@users.noreply.github.com>
2021-09-28 16:59:16 -0400

    Correct path to MongoDB
    bookStore2 to Bookstore2

--- databaseAccess.js
+++ databaseAccess.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://test:123@cen4010group8.vftre.mongodb.net/bookStore2?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://test:123@cen4010group8.vftre.mongodb.net/Bookstore2?retryWrites=true&w=majority'
 }

d54ab82d861195c1a9c77345c3105bc89ff2f863
agrim002 <82631309+agrim002@users.noreply.github.com>
2021-10-05 17:27:21 -0400

    Updated all author route functionality

--- routes/authorRoute.js
+++ routes/authorRoute.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const Author = require('../schemas/authorSchema');
 const router = express.Router();
-Author = require('../schemas/authorSchema');
 
+//render EJS to view form to create author
 router.get('/create', (req,res) => res.render('CreateAuthor'));
 
+//create author
+router.post('/create', (req,res) => {
+    var authorName = {
+        userFirst: req.body.authorFirst,
+        userLast: req.body.authorLast
+    }
+    var authorBiography = req.body.authorBiography;
+    var authorPublisher = req.body.authorPublisher;
+
+    const newAuthor = new Author({
+        authorName,
+        authorBiography,
+        authorPublisher
+    });
+    
+    newAuthor.save((err,newAuthor) => {
+        if (err) return console.log(err);
+        console.log(newAuthor);
+        res.send("Author was created!");
+    })
+});
+
+//view all authors
 router.get('/view', (req, res) => {
     Author.find() 
         .then((result) => {

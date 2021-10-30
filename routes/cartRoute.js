afa16283dd835dd2615f1d058f2c30bd656e0a5f
brianag010 <60762069+brianag010@users.noreply.github.com>
2021-10-29 21:48:55 -0400

    More basic cart functionality (add, remove, view)
    Added basic implementations for adding and removing books to a unique cart.
    
    Added basic implementation for viewing current books in a cart (currently in fancy .ejs form as opposed to raw JSON).

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -20,6 +20,15 @@ router.use(express.json());
 
 
 /// Routes
+/*	GET call for cart/.  
+	Redirects to cart/home view 
+    May move/merge with anothe route */
+router.get('/', function (req,res) 
+    {
+        res.redirect('home');
+    }
+);
+
 
 /*	GET call for cart/home.  
 	Renders the cartHome view and passes the amount of current carts stored */
@@ -62,10 +71,32 @@ router.post('/create',
     }
 );
 
-/*	POST call for cart/view.  
+/*	GET call for cart/view.  
 	Reads data in req.body for variables to access a cart. 
 	If the cart exists, user is redirected to the view of the cart (/cart/:cart_id/view)
 	Currently this only checks for a user_email */
+router.get('/view', 
+    function (req,res) 
+    {
+        var user_email = req.body.user_email;
+        var cart_id = get_index(user_email);
+
+		//cheap check to see if element exists in array, else send an error message 
+        if (get_index != null)
+        {
+            res.redirect('./' + cart_id + '/view');
+        }
+        else
+        {
+            res.send("Error, a cart with ID:\'" + cart_id +"\' does not exist");   
+        }
+    }
+);
+/*	POST call for cart/view.  
+	Reads data in req.body for variables to access a cart. 
+	If the cart exists, user is redirected to the view of the cart (/cart/:cart_id/view)
+	Currently this only checks for a user_email
+    POST request may me unnecessary  */
 router.post('/view', 
     function (req,res) 
     {
@@ -87,32 +118,136 @@ router.post('/view',
 );
 
 /*	GET call for cart/:cart_id/view.  
-	Renders the cartView based on the specified cart_id from the cart_list array */
-router.get('/:cart_id/view', 
-    function (req,res) 
+	Renders the cartView based on the specified cart_id from the cart_list array.
+    Also lists the current books in the cart */
+router.get('/:cart_id/view', cart_view_get_route);
+    function cart_view_get_route(req,res) 
     {
         var cart_id = req.params.cart_id;
 
         //cheap check to see if element exists in array 
-        if ((cart_id < cart_list.length ) && (cart_id >= 0))
-        {
+        if (cart_list[cart_id] != null)
+        { 
             var user_id = cart_list[cart_id].user_id;
-            res.render('cartView', {user_id : user_id, cart_id : cart_id });
+
+            /* Async data retrieval from async view_books method
+               Rendering waits on the return of cart_contents */
+            cart_list[cart_id].view_books()            
+            .then
+            (
+                function(cart_contents)
+                {
+                    if(req.params.json == true)
+                    {
+                        res.send(cart_contents);
+                    }
+                    else
+                    {
+                        res.render('cartView', {user_id : user_id, cart_id : cart_id, book_list : cart_contents});
+                    }
+                }
+            )
+            .catch
+            (
+                function(promise)
+                {
+                    console.log("Error:" + promise);
+                    return promise;
+                }
+            );
         }
         else
         {
             res.send("Error, a cart with ID:\'" + cart_id +"\' does not exist");
         }
     }
-);
 
-router.get('/addBook', (req,res) => res.send('Add Book to Shopping Cart'));
 
-router.get('/get', (req,res) => res.send('Display all items in shopping cart'));
+///////////////// new HTTP calls, update comments
 
-router.get('/removeBook', (req,res) => res.send('Remove book from shopping Cart'));
+/*	GET call for cart/add_book.  
+	Currently all this does is render the cartAddBook view */
+router.get('/add_book', add_book_get_route);
+router.get('/addBook', add_book_get_route);
+    function add_book_get_route(req,res) 
+    {
+        res.render('cartAddBook');
+    }
+/*	POST call for cart/add_book.  
+	Reads data in req.body for variables to access a cart and add a book based on user email and book ISBN. 
+	If the book was successfully added, the view of the user is refreshed. 
+	Ideally, this request is called from the view cart page */
+router.post('/add_book', add_book_post_route);
+router.post('/addBook', add_book_post_route);
+    function add_book_post_route(req,res) 
+    {
+        var book_ISBN = req.body.bookISBN;
+        var user_email = req.body.user_email;
+
+        // calculate cart_id from user email
+        var cart_id = get_index(user_email);
+
+        // Wait for book to be added to cart before refreshing the current view
+        cart_add_book(req, res, cart_id, book_ISBN)
+        .then
+        (
+            function(book_added_success)
+            {
+                if (book_added_success)
+                {
+                    // Refresh current view
+                    res.redirect(req.get('referer'));
+                }
+            }
+
+        );
+    }
+
+/*	GET call for cart/remove_book.  
+	Currently all this does is render the cartRemoveBook view */
+router.get('/remove_book', remove_book_get_route);
+router.get('/removeBook', remove_book_get_route);
+    function remove_book_get_route(req,res) 
+    {
+        res.render('cartRemoveBook');
+    }
+
+/*	POST call for cart/remove_book.  
+	Reads data in req.body for variables to access a cart and remove a book based on user email and book ISBN. 
+	If the book was successfully removed, the view of the user is refreshed. 
+	Ideally, this request is called from the view cart page */
+router.post('/remove_book', remove_book_post_route);
+router.post('removeBook', remove_book_post_route);
+    function remove_book_post_route(req,res) 
+    {
+        var book_ISBN = req.body.bookISBN;
+        var user_email = req.body.user_email;
+
+        // Calculate cart_id from user email
+        var cart_id = get_index(user_email);
+
+        // Wait for book to be removed from cart before refreshing the current view
+        cart_remove_book(req, res, cart_id, book_ISBN)
+        .then
+        (
+            function(book_removed_success)
+            {
+                if (book_removed_success)
+                {
+                    // Refresh current view
+                    res.redirect(req.get('referer'));
+                }
+            }
+        );
+    }
+
+//old routes
+//router.get('/addBook', (req,res) => res.send('Add Book to Shopping Cart'));
+//router.get('/get', (req,res) => res.send('Display all items in shopping cart'));
+//router.get('/removeBook', (req,res) => res.send('Remove book from shopping Cart'));
 
 module.exports = router;
+
 
 
 /// Functions
@@ -159,6 +294,21 @@ function check_existing(user_id)
     return existing;
 } 
 
+/*  Checks if a given cart id exists
+    may not be necessary? */
+function check_cart_exists(cart_id)
+{
+    var cart_exists = false;
+
+    if (cart_list[cart_id] != null)
+    {
+        cart_exists = true;
+    }
+
+    return cart_exists;
+
+}
+
 /* Returns the array index of a given user_id in cart_list */
 function get_index(user_id)
 {
@@ -175,3 +325,81 @@ function get_index(user_id)
 
     return id_index;
 } 
+
+/*  Returns the array index of a given book ISBN in book_list in a cart */
+function get_book_index(cart_id, book_ISBN)
+{
+    var id_index = null;
+
+    for(index = 0; index < cart_list[cart_id].book_list.length; index++)
+    {
+        if(cart_list[cart_id].book_list[index] == book_ISBN)
+        {
+            id_index = index;
+            return id_index;
+        }
+    }
+
+    return id_index;
+} 
+
+
+/* Add the given book ISBN to the given cart id */ 
+async function cart_add_book(req, res, cart_id, book_ISBN)
+{
+    var book_added = false;
+
+    //cheap check to see if element exists in array, else send an error message 
+    if (check_cart_exists(cart_id) == true)
+    {
+        // Check if book exists
+        var book_exists = await cart_list[cart_id].check_book_exists(book_ISBN);
+
+        if(book_exists == true)
+        {
+            cart_list[cart_id].add_book(book_ISBN);
+            book_added = true;
+        
+            //debug
+            //console.log("success!");
+            //console.log(cart_list[cart_id].view_books());
+            //console.log("testing");
+            //console.log(cart_list[cart_id]);
+        }
+        else
+        {
+            res.send("Error, a book with ISBN:\'" + book_ISBN +"\' does not exist");
+        }
+    }
+    else
+    {
+        res.send("Error, a cart with ID:\'" + cart_id +"\' does not exist");   
+    }
+
+    return book_added;
+}
+
+/* Remove the given book ISBN from the given cart id */ 
+async function cart_remove_book(req, res, cart_id, book_ISBN)
+{
+    var book_removed = false;
+
+    //cheap check to see if element exists in array, else send an error message 
+    if (check_cart_exists(cart_id) == true)
+    {
+        book_removed = cart_list[cart_id].remove_book(book_ISBN);
+
+        if(book_removed == false)
+        {
+            res.send("Error, a book with ISBN:\'" + book_ISBN +"\' was not in the cart");
+        }
+    }
+    else
+    {
+        res.send("Error, a cart with ID:\'" + cart_id +"\' does not exist");   
+    }
+
+    return book_removed;
+}
+
+
--- schemas/cartSchema.js
+++ schemas/cartSchema.js
@@ -3,6 +3,9 @@ CartSchema.js
 I basically moved Cart.js here 
 */
 
+// Import book object for integration 
+var mongoose_book = require('../schemas/bookSchema');
+
 // Main Cart Object 
 function Cart_Object(user_id)
 {
@@ -10,35 +13,108 @@ function Cart_Object(user_id)
     this.book_list = new Array();
 }
 
+
 // test function
-Cart_Object.prototype.test = function()
+Cart_Object.prototype.test = async function()
 {
+    //var ISBN = '1234';
+    //console.log(mongoose_book.find()); 
+    var test_book = await mongoose_book.findOne({bookISBN: ISBN});
+    console.log(test_book);
+
     return this.user_id;
+};
+
+
+// Get data from a book in mongodb using its ISBN value
+Cart_Object.prototype.get_book = async function(book_isbn)
+{
+    var book_data = await mongoose_book.findOne({bookISBN: book_isbn});
+
+    console.log("["+ book_isbn +"] Found book \"" + book_data.bookName +"\"");
+
+    return book_data;
+};
+
+// Add book ISBN value to array
+Cart_Object.prototype.add_book = function(book_ISBN)
+{
+    this.book_list.push(book_ISBN);
+};
+
+// Check if the given ISBN value coresponds to an existing book in the database
+Cart_Object.prototype.check_book_exists = async function(book_isbn)
+{
+    var book_exists = false;
+
+    var book_data = await mongoose_book.findOne({bookISBN: book_isbn});
+
+    if(book_data != null)
+    {
+        book_exists = true;
+    }
+    else
+    {
+        book_exists = false;
+    }
+
+    console.log("Valid ISBN "+ book_isbn +": "+ book_exists);
+
+    return book_exists;
+};
+
+/*  Returns the array index of a given book ISBN in book_list in a cart */
+Cart_Object.prototype.get_book_index = function(book_ISBN)
+{
+    var id_index = null;
+
+    for(index = 0; index < this.book_list.length; index++)
+    {
+        if(this.book_list[index] == book_ISBN)
+        {
+            id_index = index;
+            return id_index;
+        }
+    }
+
+    return id_index;
+} 
+
+/*  Removes a book from book_list array based on given ISBN value */
+Cart_Object.prototype.remove_book = function(book_ISBN)
+{
+    var book_removed = false;
+    var book_index = this.get_book_index(book_ISBN);
+
+    if(book_index != null)
+    {
+        this.book_list.splice(book_index, 1);
+        book_removed = true;
+
+        //debug
+        console.log("Removed book with ISBN " +  book_ISBN);
+    }
+
+    return book_removed;
+} 
+
+/* Generate a detailed list of books based on the ISBN values in the book_list array and the coresponding data in mongodb */
+Cart_Object.prototype.view_books = async function()
+{
+    var book_list_detailed = new Array();
+
+    // Loop through book list array and get book data for each ISBN
+    for(index = 0; index < this.book_list.length; index++ )
+    {
+        book_list_detailed.push(await this.get_book(this.book_list[index]));
+    }
+
+    //console.log(book_list_detailed);
+
+    return book_list_detailed;
 };
 
 
 // Export Object
 module.exports = Cart_Object;
 
-
-/*
-const mongoose = require('mongoose');
-
-const cartSchema = new mongoose.Schema({
-    cartArray: {
-        type: Array,
-        required: true
-    },
-    cartUser:{
-        type: Object,
-        required: true
-    }
-});
-
-
-const Cart = module.exports = mongoose.model('Cart', cartSchema);
-
-module.exports.get = function (callback, limit) {
-    Cart.find(callback).limit(limit);
-}
-*/

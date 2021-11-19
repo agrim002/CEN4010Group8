ae2364f989089190f4afad2e02b0d163c559194e
PhantomShow <90368414+PhantomShow@users.noreply.github.com>
2021-11-18 21:10:01 -0500

    fixed view by genre

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -57,7 +57,7 @@ router.get('/view', (req, res) => {
 router.get('/view/genre/', async (req, res) => res.render('viewByGenre'));
 router.get('/view/genre/:genre', async (req, res) => {
     const genre = req.params.genre;
-    const book = await Book.find({allRatings: genre});
+    const book = await Book.find({genre: genre});
     res.send(book);
 });
 
